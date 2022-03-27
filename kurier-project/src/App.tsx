5765c25902ed03334cee9ffc8e5e1bc13ea6209e
L42ARO <alvaor512@usf.edu>
2022-03-27 09:15:54 -0400

    other other test

--- kurier-project/src/App.tsx
+++ kurier-project/src/App.tsx
@@ -11,8 +11,10 @@ import './App.css';
 
 function App() {
   const [count, setCount] = React.useState(0);
-  function handleClick() {
+  function handleClick (e: React.ChangeEvent<any>) {
+    e.preventDefault();
     setCount(count + 1);
+    console.log("click!");
   }
   return (
     <div>
