b803801933065921625b90cb9d8721aa96a945c1
L42ARO <alvaor512@usf.edu>
2022-03-27 02:01:07 -0400

    pivoted to react

--- js/bone.js
+++
@@ -1,5 +0,0 @@
-console.log("hallo");
-var doge = document.getElementsByClassName("doge");
-doge[0].addEventListener('input', function (evt) {
-    console.log(doge[0].value)
-});
--- js/negotiator.js
+++
@@ -1,7 +0,0 @@
-var currbid = document.getElementsByClassName("curr-bid");
-var accept = document.getElementsByClassName("accept");
-var val_currbid;
-accept[0].addEventListener('click',function(){
-    val_currbid=currbid[0].innerHTML;
-    console.log(val_currbid);
-})
