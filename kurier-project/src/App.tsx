5108b176f805595888e81b470ec12e355eb98aa6
L42ARO <alvaor512@usf.edu>
2022-03-27 04:27:38 -0400

    lil fix to app.tsx

--- kurier-project/src/App.tsx
+++ kurier-project/src/App.tsx
@@ -12,7 +12,7 @@ import './App.css';
 function App() {
   return (
     <Router>
-      {/* <nav className="navbar navbar-expand-lg navbar-dark navbar-custom">
+      <nav className="navbar navbar-expand-lg navbar-dark navbar-custom">
         <div className="container-fluid">
           <a className="navbar-brand" aria-label="home page" href="/">CockroachDB + TypeScript</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,7 +26,7 @@ function App() {
             </ul>
           </div>
         </div>
-      </nav> */}
+      </nav>
 
       <div className="container page-content">
         <Routes>
