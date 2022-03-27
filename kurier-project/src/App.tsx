762da3affcbe6fe4389dee3f76013420cf7ce1f3
L42ARO <alvaor512@usf.edu>
2022-03-27 09:46:50 -0400

    userCredentialsTest

--- kurier-project/src/App.tsx
+++ kurier-project/src/App.tsx
@@ -9,18 +9,48 @@ import * as React from 'react';
 // import UserAuth from './UserAuth';
 import './App.css';
 
+interface User {
+  id: number,
+  firstname: string,
+  lastname: string,
+  email: string
+  password: string
+}
+
 function App() {
+  const [users, setUsers] = React.useState([] as User[]);
+  React.useEffect(() => {
+    fetch('/.netlify/functions/getUsers')
+        .then(response => response.json() as Promise<User[]>)
+        .then(data => setUsers(data));
+}, []);
   const [emailInput, setMail] = React.useState("");
+  const [pwdInput, setPwd] = React.useState("");
   function handleClick (e: React.ChangeEvent<any>) {
     e.preventDefault();
-    console.log(emailInput);
+    if(emailInput != "" && pwdInput != ""){
+      users.forEach(user => {
+        if(user.email === emailInput && user.password === pwdInput){
+          console.log("Logged in");
+        }else{
+          console.log("Wrong credentials");
+        }
+      });
+    }else{
+      console.log("No credentials");
+    }
+    
   }
-  function handleChange (e: React.ChangeEvent<HTMLInputElement>) {
+  function handleChangeEmail (e: React.ChangeEvent<HTMLInputElement>) {
     setMail(e.target.value);
+  }
+  function handleChangePassword (e: React.ChangeEvent<HTMLInputElement>) {
+    setPwd(e.target.value);
   }
   return (
     <div>
-      <input  onChange={handleChange} placeholder="Username"></input>
+      <input  onChange={handleChangeEmail} placeholder="Username"></input>
+      <input type="text" onChange={handleChangePassword} placeholder="Password"></input>
       <button onClick={handleClick}>
         Click me
       </button>
