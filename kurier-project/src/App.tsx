63ffb6c2538ee8f9e4ca70eb0e8d694e72b2453c
L42ARO <alvaor512@usf.edu>
2022-03-27 10:14:27 -0400

    lil fix for non glboal vars

--- kurier-project/src/App.tsx
+++ kurier-project/src/App.tsx
@@ -9,71 +9,78 @@ import * as React from 'react';
 // import UserAuth from './UserAuth';
 import './App.css';
 
-//USERS DATA FETCH----------------------------------------------------------------------------
-interface User {
-  id: number,
-  firstname: string,
-  lastname: string,
-  email: string
-  password: string
-}
-const [logedIN, setLogedIN] = React.useState(false);
-const [users, setUsers] = React.useState([] as User[]);
-React.useEffect(() => {
-  fetch('/.netlify/functions/getUsers')
-      .then(response => response.json() as Promise<User[]>)
-      .then(data => setUsers(data));
-}, []);
-const [currentUser, setCurrentUser] = React.useState(null as User | null);
-
-function UserAuth() {
-   // EMAIL AND PASSWORD AUTHENTICATION----------------------------------------------------------
-   const [emailInput, setMail] = React.useState("");
-   const [pwdInput, setPwd] = React.useState("");
-   const [logInVis, setLogInVis] = React.useState("d-flex");
-   const [warning, setWarning] = React.useState("");
-   function handleClick (e: React.ChangeEvent<any>) {
-     e.preventDefault();
-     if(emailInput !== "" && pwdInput !== ""){
-       users.forEach(user => {
-         if(user.email === emailInput && user.password === pwdInput){
-           setWarning("Welcome, you're logged in!");
-           setLogedIN(true);
-           setLogInVis("d-none");
-         }
-       });
-       if(!logedIN){
-         setWarning("Wrong credentials");
-       }
-     }else{
-       setWarning("No credentials");
-     }
-     
-   }
-   function handleChangeEmail (e: React.ChangeEvent<HTMLInputElement>) {
-     setMail(e.target.value);
-   }
-   function handleChangePassword (e: React.ChangeEvent<HTMLInputElement>) {
-     setPwd(e.target.value);
-   }
-   return (
-     <div className={logInVis}>
-       <input  onChange={handleChangeEmail} placeholder="Username"></input>
-       <input type="text" onChange={handleChangePassword} placeholder="Password"></input>
-       <button onClick={handleClick}>
-         Click me
-       </button>
-       <div>{warning}</div>
-     </div>
-   );
-}
-
 function App() {
- return(
-   <div>
-      <UserAuth />
-   </div>
- )
+  //USERS DATA FETCH----------------------------------------------------------------------------
+  interface User {
+    id: number,
+    firstname: string,
+    lastname: string,
+    email: string
+    password: string
+  }
+  const [logedIN, setLogedIN] = React.useState(false);
+  const [users, setUsers] = React.useState([] as User[]);
+  React.useEffect(() => {
+    fetch('/.netlify/functions/getUsers')
+        .then(response => response.json() as Promise<User[]>)
+        .then(data => setUsers(data));
+  }, []);
+  const [currentUser, setCurrentUser] = React.useState(null as User | null);
+
+  // EMAIL AND PASSWORD AUTHENTICATION----------------------------------------------------------
+  function UserAuth() {
+    const [emailInput, setMail] = React.useState("");
+    const [pwdInput, setPwd] = React.useState("");
+    const [logInVis, setLogInVis] = React.useState("d-flex");
+    const [warning, setWarning] = React.useState("");
+    function handleClick (e: React.ChangeEvent<any>) {
+      e.preventDefault();
+      if(emailInput !== "" && pwdInput !== ""){
+        users.forEach(user => {
+          if(user.email === emailInput && user.password === pwdInput){
+            setWarning("Welcome, you're logged in!");
+            setLogedIN(true);
+            setLogInVis("d-none");
+          }
+        });
+        if(!logedIN){
+          setWarning("Wrong credentials");
+        }
+      }else{
+        setWarning("No credentials");
+      }
+      
+    }
+    function handleChangeEmail (e: React.ChangeEvent<HTMLInputElement>) {
+      setMail(e.target.value);
+    }
+    function handleChangePassword (e: React.ChangeEvent<HTMLInputElement>) {
+      setPwd(e.target.value);
+    }
+    return (
+      <div className={logInVis}>
+        <input  onChange={handleChangeEmail} placeholder="Username"></input>
+        <input type="text" onChange={handleChangePassword} placeholder="Password"></input>
+        <button onClick={handleClick}>
+          Click me
+        </button>
+        <div>{warning}</div>
+      </div>
+    );
+  }
+  // function taskPosts() {
+  //   return (
+  //     <div>
+  //       <h1>Task Posts</h1>
+  //     </div>
+  //   );
+  // }
+  return(
+    <div>
+       <UserAuth />
+    </div>
+  )
 }
+
 
 export default App;
