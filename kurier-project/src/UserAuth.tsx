402ea7886523d6d4d7e1ac8704831d41dc60d001
L42ARO <alvaor512@usf.edu>
2022-03-27 03:18:36 -0400

    first test of new functions

---
+++ kurier-project/src/UserAuth.tsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+
+interface Leader {
+    id: number,
+    firstName: string,
+    lastName: string,
+    email: string
+}
+
+function renderLeader(leader: Leader) {
+    return <tr key={leader.id}>
+        <td>{leader.id}</td>
+        <td>{leader.firstName}</td>
+        <td>{leader.lastName}</td>
+        <td>{leader.email}</td>
+    </tr>
+}
+
+export default function UserAuth() {
+
+    const [leaders, setLeaders] = useState([] as Leader[]);
+
+    useEffect(() => {
+        fetch('/.netlify/functions/getUsers')
+            .then(response => response.json() as Promise<Leader[]>)
+            .then(data => setLeaders(data));
+    }, []);
+
+    return <>
+        <h2>UserAuth</h2>
+        {leaders.length === 0 ? 
+            <div>No leader scores to display. Would you like to <Link to="admin">add one</Link>?</div>
+        :
+            <table className="table leader-table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>LastName</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {leaders.map(l => renderLeader(l))}
+                </tbody>
+            </table>
+        }
+    </>
+}
